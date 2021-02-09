13b8c8629a8ee7651c0eaf301aef26b4be820544
Lee Dasom <33215628+LeSunny@users.noreply.github.com>
2021-02-09 13:28:56 +0900

    Create BOJ11866.java

---
+++ BOJ/BOJ11866.java
@@ -0,0 +1,36 @@
+package boj;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BOJ11866 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] info = br.readLine().split("\\s");
+		int n = Integer.parseInt(info[0]);
+		int k = Integer.parseInt(info[1]);
+		StringBuilder sb = new StringBuilder();
+		Queue<Integer> queue = new LinkedList<Integer>();
+
+		for (int i = 1; i < n + 1; i++) {
+			queue.offer(i);
+		}
+
+		sb.append("<");
+		while (!queue.isEmpty()) {
+			for (int i = 0; i < k - 1; i++) {
+				queue.offer(queue.poll());
+			}
+			sb.append(queue.poll());
+			if (queue.size() == 0)
+				sb.append(">");
+			else
+				sb.append(", ");
+		}
+		System.out.println(sb);
+		br.close();
+	}
+
+}
